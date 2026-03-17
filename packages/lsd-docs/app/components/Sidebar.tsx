ec239b51acaa1bda9360eef2ad108d44f62d21e0
Xav <bonjour@xaviers.sh>
2026-03-17 12:57:44 -0400

    refactor: remove obsolete sidebar components

--- packages/lsd-docs/app/components/Sidebar.tsx
+++
@@ -1,9 +0,0 @@
-import { SidebarContentComponent } from './SidebarContent';
-
-interface SidebarProps {
-  currentPath: string;
-}
-
-export default function Sidebar({ currentPath }: SidebarProps) {
-  return <SidebarContentComponent currentPath={currentPath} />;
-}
