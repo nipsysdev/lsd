93bfb257998c37ecf86bdba54f5cf331709e9b92
Xav <bonjour@xaviers.sh>
2026-04-07 23:13:03 -0400

    feat: update TableHead component styling

--- packages/shadcn-lsd/src/components/ui/table/TableHead.tsx
+++ packages/shadcn-lsd/src/components/ui/table/TableHead.tsx
@@ -9,7 +9,7 @@ const TableHead = React.forwardRef<HTMLTableCellElement, React.ComponentProps<'t
         ref={ref}
         data-slot="table-head"
         className={cn(
-          'lsd:h-10 lsd:px-[var(--lsd-spacing-small)] lsd:text-left lsd:align-middle lsd:font-medium lsd:whitespace-nowrap lsd:text-lsd-text-primary lsd:[&:has([role=checkbox])]:pr-0 lsd:[&>[role=checkbox]]:translate-y-[2px]',
+          'lsd:h-10 lsd:px-(--lsd-spacing-small) lsd:text-left lsd:align-middle lsd:font-bold lsd:whitespace-nowrap lsd:text-lsd-text-primary lsd:[&:has([role=checkbox])]:pr-0 lsd:*:[[role=checkbox]]:translate-y-[2px]',
           className
         )}
         {...props}
