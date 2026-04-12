c7d3b5ee2bd37631053061888f62bf55e0bc8efa
Xav <bonjour@xaviers.sh>
2026-04-12 12:48:38 -0400

    docs: update themes configuration

--- packages/lsd-docs/app/config/themes.ts
+++ packages/lsd-docs/app/config/themes.ts
@@ -1,2 +1,2 @@
-export const Themes = ['monochrome', 'teal', 'nord'] as const;
+export const Themes = ['monochrome', 'teal', 'nord', 'terracotta', 'catppuccin'] as const;
 export type Theme = (typeof Themes)[number];
